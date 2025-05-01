1d677150d7c04a2019ed9664883491297b512bc4
bitardich <bitard@xyecoc.me>
2025-05-01 19:14:26 +0900

    реализовал гуй

--- src/gui/main.py
+++ src/gui/main.py
@@ -1,1 +1,14 @@
 # -*- coding: utf-8 -*-
+
+import flet as ft
+
+class App:
+    def __init__(self):
+        pass
+
+    def main(self, page: ft.Page):
+        page.title = "telebitard"
+        page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+        
+
--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 
-# import modules
 import sys
 
 try:
     import core
+    from gui.main import App
     print("all done")
 except (ImportError, ModuleNotFoundError) as e:
     print(f"error: {e}")
     sys.exit(1)
 
+import flet as ft
 
+def main():
+    ft.app(target=App().main)
+
+if __name__ == "__main__":
+    main()
